9d77eceb6565b919b0f30636d43e5e349903aca5
POKA TCHONENG Hermann <pokatchoneng@yahoo.fr>
2024-07-23 09:39:24 +0100

    part 3 defining our data

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -61,5 +62,13 @@ class User extends Authenticatable implements MustVerifyEmail
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function posts():HasMany{
+        return $this->hasMany(Post::class);
+    }
+
+    public function comments():HasMany{
+        return $this->hasMany(Comment::class);
     }
 }
