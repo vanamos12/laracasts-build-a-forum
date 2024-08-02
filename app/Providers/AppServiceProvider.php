c81a6ecaf4aeb7602bbe543f4c68d58a3e71402a
POKA TCHONENG Hermann <pokatchoneng@yahoo.fr>
2024-08-02 10:28:20 +0100

    part 9 helpful testing helpers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         JsonResource::withoutWrapping();
-        Model::preventLazyLoading();
+        //Model::preventLazyLoading();
     }
 }
--- tests/Feature/Controllers/PostController/IndexTest.php
+++ tests/Feature/Controllers/PostController/IndexTest.php
@@ -1,13 +1,24 @@
 <?php
 
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
 use Inertia\Testing\AssertableInertia;
 
 it('should return the correct component', function(){
     $this->get(route('posts.index'))
         ->assertInertia(fn (AssertableInertia $inertia) => $inertia->component('Posts/Index', true));
+
+    
 });
 
 it('passes posts to the view', function (){
+    
+    $posts = Post::factory(3)->create();
+
     $this->get(route('posts.index'))
-    ->assertInertia(fn (AssertableInertia $inertia) => $inertia->has('posts')); 
+    ->assertHasPaginatedResource('posts', PostResource::collection($posts->reverse()));
+ 
 });
+
