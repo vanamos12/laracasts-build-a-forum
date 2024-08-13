cfaee22f7cc255b819cbb444def31688251df569
POKA TCHONENG Hermann <pokatchoneng@yahoo.fr>
2024-08-13 15:44:02 +0100

    part 15 the comment form

---
+++ resources/js/Components/TextArea.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+defineProps({
+    modelValue: String,
+});
+
+defineEmits(['update:modelValue']);
+
+const input = ref(null);
+
+onMounted(() => {
+    if (input.value.hasAttribute('autofocus')) {
+        input.value.focus();
+    }
+});
+
+defineExpose({ focus: () => input.value.focus() });
+</script>
+
+<template>
+    <textarea
+        ref="input"
+        class="block w-full border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+    ></textarea>
+</template>
--- resources/js/Pages/Posts/Show.vue
+++ resources/js/Pages/Posts/Show.vue
@@ -10,6 +10,15 @@
             <div class="mt-12">
                 <h2 class="text-xl font-semibold">Comments</h2>
 
+                <form v-if="$page.props.auth.user" @submit.prevent="addComment" class="mt-4">
+                    <div>
+                        <InputLabel for="body" class="sr-only">Comment</InputLabel>
+                        <TextArea id="body" rows="4" v-model="commentForm.body" placeholder="Speaks yoru mind spocks..."/>
+                        <InputError :message="commentForm.errors.body" class="mt-1"/>
+                    </div>
+
+                    <PrimaryButton type="submit" class="mt-3" :disable="commentForm.processing">Add Comment</PrimaryButton>
+                </form>
                 <ul class="divide-y mt-4">
                     <li class="px-2 py-4" v-for="comment in comments.data" :key="comment.id">
                         <Comment :comment="comment" />
@@ -26,13 +35,28 @@
 
 import Comment from '@/Components/Comment.vue';
 import Container from '@/Components/Container.vue';
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
 import Pagination from '@/Components/Pagination.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextArea from '@/Components/TextArea.vue';
+import TextInput from '@/Components/TextInput.vue';
 import AppLayout from '@/Layouts/AppLayout.vue';
 import { relativeDate } from '@/Utilities/date';
+import { useForm } from '@inertiajs/vue3';
 import { computed } from 'vue';
 
 const props = defineProps(['post', 'comments']);
 
 const formattedDate = computed(() => relativeDate(props.post.created_at));
 
+const commentForm = useForm({
+    body: '',
+})
+const addComment = () => {
+    commentForm.post(route('posts.comments.store', props.post.id), {
+        preserveScroll: true,
+        onSuccess: () => commentForm.reset(),
+    })
+};
 </script>
