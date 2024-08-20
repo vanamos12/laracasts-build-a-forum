4f3094c4afe1d8e3649bf4aceaf2623e272af23a
POKA TCHONENG Hermann <pokatchoneng@yahoo.fr>
2024-08-20 12:14:30 +0100

    part 22 useful user feedback toasts

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -42,7 +42,7 @@ class CommentController extends \Illuminate\Routing\Controller
             'user_id' => $request->user()->id
         ]);    
 
-        return to_route('posts.show', $post);
+        return to_route('posts.show', $post)->banner('Comment added!');
     }
 
     
@@ -59,7 +59,8 @@ class CommentController extends \Illuminate\Routing\Controller
 
         $comment->update($data);  
 
-        return to_route('posts.show', ['post' => $comment->post_id, 'page' => $request->query('page')]);
+        return to_route('posts.show', ['post' => $comment->post_id, 'page' => $request->query('page')])
+        ->banner('Comment updated!');
     }
 
     /**
@@ -72,6 +73,7 @@ class CommentController extends \Illuminate\Routing\Controller
 
         $comment->delete();
 
-        return to_route('posts.show', ['post' => $comment->post_id, 'page' => $request->query('page')]);
+        return to_route('posts.show', ['post' => $comment->post_id, 'page' => $request->query('page')])
+        ->banner('Comment deleted!');
     }
 }
